b9a5a0a9f345afbcc8a4715844fc5c7a95ee0cdd
djalexkidd <alexpyr@outlook.fr>
2022-01-20 14:46:14 +0100

    ultimate bruh momento

---
+++ script.js
@@ -0,0 +1,65 @@
+var div = document.getElementById('test');
+
+function add1() {
+    div.innerHTML += 1;
+}
+
+function add2() {
+    div.innerHTML += 2;
+}
+
+function add3() {
+    div.innerHTML += 3;
+}
+
+function add4() {
+    div.innerHTML += 4;
+}
+
+function add5() {
+    div.innerHTML += 5;
+}
+
+function add6() {
+    div.innerHTML += 6;
+}
+
+function add7() {
+    div.innerHTML += 7;
+}
+
+function add8() {
+    div.innerHTML += 8;
+}
+
+function add9() {
+    div.innerHTML += 9;
+}
+
+function add0() {
+    div.innerHTML += 0;
+}
+
+function equal() {
+    eval(div.innerHTML);
+}
+
+function allclear() {
+    div.innerHTML = "";
+}
+
+function addmultiplicator() {
+    div.innerHTML += "*";
+}
+
+function adddifference() {
+    div.innerHTML += "-";
+}
+
+function addsum() {
+    div.innerHTML += "+";
+}
+
+function adddivide() {
+    div.innerHTML += "/";
+}
